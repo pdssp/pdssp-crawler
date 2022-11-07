6ff9a9cc8315c060ec69b793a0145c03851cc3e0
nmanaud <nmanaud@gmail.com>
2022-11-07 12:06:53 +0100

    setting up

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # pdssp-crawler
 PDSSP Crawler Component
 
+### Installation
+
+```
+git clone 
+pip install .
+```
 
 ### Test
 
 ```
 pytest --cov=crawler
 ```
+
+### Documentation
+
+- Build: from `docs/` directory, run `make html`.
+- Publish: from root, run `ghp-import -n -p -f docs/build/html`
--- crawler/config.py
+++ crawler/config.py
@@ -1,8 +1,8 @@
 """PDSSP Crawler configuration module."""
 
-SOURCE_DATA_DIR = '/Users/nmanaud/workspace/pdssp/pdssp-crawler-data'
+SOURCE_DATA_DIR = '/Users/nmanaud/workspace/pdssp/crawler-data'
 STAC_DATA_DIR = '/Users/nmanaud/workspace/pdssp/pdssp-stac-repo'
 LOCAL_REGISTRY_YAML_FILE = '/Users/nmanaud/workspace/pdssp/pdssp-crawler/data/registry/catalog.yaml'
-REGISTRY_GITHUB_REPOSITORY = 'https://github.com/nmanaud/pdssp-crawler/tree/main/data/registry'
-STAC_GITHUB_REPOSITORY = 'https://github.com/nmanaud/pdssp-stac-repo'
+REGISTRY_GITHUB_REPOSITORY = 'https://github.com/pdssp/pdssp-crawler/tree/main/data/registry'
+STAC_GITHUB_REPOSITORY = 'https://github.com/pdssp/pdssp-stac-repo'
 STAC_CATALOG_ENDPOINT = 'https://pdssp.ias.universite-paris-saclay.fr/catalog'
